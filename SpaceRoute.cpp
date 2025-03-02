90a9c25abe8deb6ac9953b74993066fbb780ec9b
Sean Lumahan <seanlumahan910@gmail.com>
2025-03-01 21:45:40 -0800

    Implemented all functions minus a few edge cases

--- SpaceRoute.cpp
+++ SpaceRoute.cpp
@@ -32,6 +32,7 @@ class SpaceRoute {
 private:
     Node<T>* head;
     Node<T>* tail;
+    int routeLength;
 
 public:
     SpaceRoute();  // Constructor
@@ -41,28 +42,65 @@ public:
         Node<T>* newWayPoint = new Node<T>(data);
         newWayPoint->next = head;
         head = newWayPoint;
+        routeLength++;
     }
 
     void addWaypointAtEnd(T& data) {
         Node<T>* newWayPoint = new Node<T>(data);
         newWayPoint->prev = tail;
         tail = newWayPoint;
+        routeLength++;
     };
-    void addWaypointAtIndex(int index, T& data);
+    void addWaypointAtIndex(int index, T& data) {
+        if (index == 0) {
+            addWaypointAtBeginning(data);
+        } else if (index == routeLength) {
+            addWaypointAtEnd(data);
+        } else {
+            Node<T>* newWayPoint = new Node<T>(data);
+            Node<T>* afterWayPoint = getWaypoint(index);
+            Node<T>* priorWayPoint = afterWayPoint->prev;
+
+            newWayPoint->next = afterWayPoint;
+            newWayPoint->prev = priorWayPoint;
+            priorWayPoint->next = newWayPoint;
+            afterWayPoint->prev = newWayPoint;
+            routeLength++;
+        }
+
+    };
 
     void removeWaypointAtBeginning() {
         Node<T>* temp = head;
         head = temp->next;
         delete temp;
+        routeLength--;
     }
 
     void removeWaypointAtEnd() {
         Node<T>* temp = tail;
         tail = temp->prev;
         delete temp;
+        routeLength--;
     }
 
-    void removeWaypointAtIndex(int index);
+    void removeWaypointAtIndex(int index) {
+        if (index == 0) {
+            removeWaypointAtBeginning();
+        } else if (index == routeLength) {
+            removeWaypointAtEnd();
+        } else {
+            Node<T>* targetedWayPoint = getWaypoint(index);
+            Node<T>* priorWayPoint = targetedWayPoint->prev;
+            Node<T>* afterWayPoint = targetedWayPoint->next;
+
+            priorWayPoint->next = afterWayPoint;
+            afterWayPoint->prev = priorWayPoint;
+
+            delete targetedWayPoint;
+            routeLength--;
+        }
+    };
 
     void traverseForward() {
         Node<T>* temp = head;
@@ -80,17 +118,31 @@ public:
         }
     }
 
-    Node<T>* getWaypoint(int index);
-    void setWaypoint(int index, T& data);
-    void print(){
-
-            Node<T>* current = head;
-            while (current) {
-                current->print();
-                current = current->next;
-            }
-            cout << endl;
+    Node<T>* getWaypoint(int index) {
+        Node<T>* temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp->next;
         }
+        return temp;
+    };
+
+
+    void setWaypoint(int index, T& data) {
+        Node<T>* temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp->next;
+        }
+        temp->data = data;
+    };
+
+    void print(){
+        Node<T>* current = head;
+        while (current) {
+            current->print();
+            current = current->next;
+        }
+        cout << endl;
+    }
 
 };
 
